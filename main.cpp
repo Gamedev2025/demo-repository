63619e088887bb849155aead81d1a834560373b4
fayek2025 <fayekahmedrahat90@gmail.com>
2023-09-28 11:10:49 +0600

    Fayek tui takla

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
 
+using ll = long long int;
+using ull = unsigned long long int;
+using namespace std;
+const ll modular = 1e9 + 7;
+const double pi = 22 / 7;
+
+void solve()
+{
+    
+    
+}
 
 int main()
 {
-    int a;
-    int b;
-    int c;
-    cin >> a;
-    cout<<"hello";
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    n = 1;
+    cin >> n;
+    for (ll i = 1; i <= n; ++i)
+    {
+        // cout << "Case " << i << ": ";
+        solve();
+    }
+    return 0;
 }
