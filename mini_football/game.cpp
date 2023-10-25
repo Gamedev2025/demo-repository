8d012ae430fe865c5caf5855717455e5b04a83f0
Fayek Ahmed <63711263+fayek2025@users.noreply.github.com>
2023-10-25 22:57:11 +0600

    Add files via upload

---
+++ mini_football/game.cpp
@@ -0,0 +1,232 @@
+#include "game.h"
+
+#include<SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+GameEntity* object1;
+GameEntity* object2;
+GameEntity* object3;
+//tile* object4;
+GameEntity*object4;
+
+
+    int width = 640;
+    int height = 480;
+    SDL_Surface* tile = IMG_Load("image/football-3.png") ;
+    SDL_Texture* texture ;
+Game::Game()
+{
+    //This where we initialize everything
+    if((SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)==-1)) { 
+        printf("Could not initialize SDL: %s.\n", SDL_GetError());
+        exit(-1);
+    }
+   
+     SDL_CreateWindowAndRenderer( width , height , 0 , &window , &renderer);
+     SDL_SetWindowTitle(window , "First game!!");
+     isGameRunning = true;
+     
+    
+    object1 = new GameEntity(renderer,"./image/square_barrel.png");
+    object2 = new GameEntity(renderer,"./image/square_barrel.png");
+    object3 = new GameEntity(renderer , "image/walking_cycle.png");
+    //object4 = new GameEntity(renderer , "./image/football_field2.png");
+     tile ;
+    texture = SDL_CreateTextureFromSurface(renderer , tile);
+     loop();
+}
+
+Game::~Game()
+{
+    //Freeing all the allocated part
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_FreeSurface(tile);
+    SDL_DestroyTexture(texture);
+    SDL_Quit();
+}
+void Game::loop()
+{
+    while(isGameRunning)
+    {
+        
+        Uint32 button;
+        button = SDL_GetMouseState(&m_mouseX,&m_mouseY);
+        
+            render();
+            input();
+            update();
+
+        
+    }
+}
+
+void Game::render()
+{
+    
+
+    SDL_SetRenderDrawColor(renderer , 103, 117, 105, 0.66);
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 0; 
+    rect.w = width;
+    rect.h = height;
+
+    
+    SDL_RenderFillRect(renderer, &rect);
+    TexturedRectangle rectangle(renderer , "./image/square_barrel.png");
+    
+    //object4->GetTexturedRectangle().SetPosition(300 , 300 ,100 , 100);
+   
+    object1->GetTexturedRectangle().SetPosition(key_x , key_y , 100 , 100);
+    object2->GetTexturedRectangle().SetPosition(300 , 300 , 100 , 100);
+    
+    object3->GetTexturedRectangle().draw(c_x , c_y , 125 , 125);
+    object3->GetTexturedRectangle().playFrame(66 , 84 , 154 , 124 , frameNumber);
+    object3->GetTexturedRectangle().updateAnimation(renderer);
+    object1->Render();
+    object2->Render();
+    int tile[40][60];
+
+
+
+    for (int x = 0 ; x < 40 ; x++)
+    {
+        for (int y = 0 ; y < 60 ; y++)
+        {
+            Tile_Sets[x][y].x = x*32;
+            Tile_Sets[x][y].y = y*32;
+            Tile_Sets[x][y].w = 32;
+            Tile_Sets[x][y].h = 32;
+
+             
+            
+                 SDL_RenderCopy(renderer , texture , &Tile_Sets[x][y] , &Tile_Sets[x][y]);
+              
+
+        }
+    }
+    //  character.playFrame(66 , 84 , 154 , 124 , frameNumber);
+    //  character.updateAnimation(renderer);
+   
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(100);
+}
+
+
+
+void Game::input()
+{
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            isGameRunning = false;
+        }
+
+        if (event.type == SDL_KEYDOWN)
+        {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                isGameRunning = false;
+            }
+
+            if (event.key.keysym.sym == SDLK_w) 
+            {
+                key_y++;
+                c_y++;
+                frameNumber++;
+                if (frameNumber > 4)
+                {
+                frameNumber = 0;
+                }
+             
+            }
+
+            //frameNumber = 0;
+
+            if (event.key.keysym.sym == SDLK_s) 
+            {
+                key_y--;
+                c_y--;
+                frameNumber++;
+                if (frameNumber > 4)
+                {
+                    frameNumber = 0;
+                }
+               
+            }
+
+            //frameNumber = 0;
+            if (event.key.keysym.sym == SDLK_a) 
+            {
+                key_x--;
+                c_x--;
+                frameNumber++;
+                if (frameNumber > 4)
+                {
+                    frameNumber = 0;
+                }
+               
+            }
+            //frameNumber = 0;
+            if (event.key.keysym.sym == SDLK_d) 
+            {
+                key_x++;
+                c_x++;
+
+                frameNumber++;
+                if (frameNumber > 4)
+                {
+                    frameNumber = 0;
+                }
+              
+            }
+            //frameNumber = 0;
+            if (key_x < 0) {
+            key_x = 0;
+        } else if (key_x > width- 100) {
+            key_x = width - 100;
+        }
+
+        if (key_y < 0) {
+            key_y = 0;
+        } else if (key_y > height - 100) {
+            key_y = height - 100;
+        }
+            
+        }
+
+        if(event.button.button == SDL_BUTTON_LEFT)
+        {
+           if(object2->GetTexturedRectangle().IsColliding(object1->GetTexturedRectangle()))
+           {
+                std::cout << "Is colliding" << std::endl;
+           }else
+           {
+               std::cout << "Not colliding" << std::endl;
+           }
+        }
+    }
+}
+
+
+int Game::GetMouseX()
+{
+    return m_mouseX;
+}
+
+int Game::GetMouseY()
+{
+    return m_mouseY;
+}
+
+
+void Game::StopAppLoop()
+{
+   isGameRunning = false; 
+};
+
